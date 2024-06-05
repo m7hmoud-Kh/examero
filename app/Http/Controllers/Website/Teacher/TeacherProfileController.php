0830f57de24a7c0ccacd7170b27c4bb3e7d35ffc
mahmoud <khairymahmoud795@gmail.com>
2024-06-05 23:23:57 +0300

    make Profile For Student And Teacher

---
+++ app/Http/Controllers/Website/Teacher/TeacherProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Website\Teacher;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\Teacher\UpdateTeacherRequest;
+use App\Http\Requests\Website\Teacher\ChangePasswordRequest;
+use App\Http\Requests\Website\Teacher\UpdateProfileTeacherRequest;
+use App\Models\Teacher;
+use App\Services\ProfileAuthService;
+use Illuminate\Http\Request;
+
+class TeacherProfileController extends Controller
+{
+    public $profileAuthService;
+    public function __construct(ProfileAuthService $profileAuthService)
+    {
+        $this->profileAuthService = $profileAuthService;
+    }
+
+    public function userProfile()
+    {
+        return $this->profileAuthService->userProfile('teacher');
+    }
+
+    public function update(UpdateProfileTeacherRequest $request)
+    {
+        return $this->profileAuthService->update($request,new Teacher(),'teacher');
+    }
+
+    public function changePassword(ChangePasswordRequest $request)
+    {
+        return $this->profileAuthService->changePassword($request,new Teacher(),'teacher');
+    }
+
+}
