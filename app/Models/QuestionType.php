775435ddbac0155f9a44b49d5fe3d9718065f4b3
mahmoud <khairymahmoud795@gmail.com>
2024-06-14 19:58:08 +0300

    Remove Author activirylog when seedin

--- app/Models/QuestionType.php
+++ app/Models/QuestionType.php
@@ -17,15 +17,19 @@ class QuestionType extends Model
 
     public function tapActivity(Activity $activity, string $eventName)
     {
-        $causer = Admin::role(['manager','supervisor','owner'])->find(Auth::guard('admin')->user()->id);
-        $activity->description = "Question Type has been {$eventName} by " . ($causer ? $causer->email : 'an unknown user');
-        $activity->properties = $activity->properties->merge([
-            'causer_email'=>$causer ? $causer->email : 'unknown',
-            'fullName' =>
-            $causer ? $causer->first_name . ' ' . $causer->last_name : 'unkown',
-            'role_user' => $causer ? $causer->roles[0]->name : 'unknown',
-            'event' => $activity->event,
-        ]);
+        if(Auth::guard('admin')->user()){
+            $causer = Admin::role(['manager','supervisor','owner'])->find(Auth::guard('admin')->user()->id);
+            $activity->description = "Question Type has been {$eventName} by " . ($causer ? $causer->email : 'an unknown user');
+            $activity->properties = $activity->properties->merge([
+                'causer_email'=>$causer ? $causer->email : 'unknown',
+                'fullName' =>
+                $causer ? $causer->first_name . ' ' . $causer->last_name : 'unkown',
+                'role_user' => $causer ? $causer->roles[0]->name : 'unknown',
+                'event' => $activity->event,
+            ]);
+        }else{
+            $activity->description = 'null';
+        }
     }
 
 
