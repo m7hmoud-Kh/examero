b72c824fe56f3f2a06cceafaba576fbdb8ef5a4a
mahmoud <khairymahmoud795@gmail.com>
2024-07-01 17:31:33 +0300

    make ActivityInArabic

--- resources/lang/ar/services.php
+++ resources/lang/ar/services.php
@@ -8,7 +8,31 @@ return [
     'password_updated' => 'تم تعديل كلمه المرور بنجاح',
     'invalid_data' => 'البيانات غير صحيحه',
     'unauth' => 'ربما البريد الاكتروني او الباسورد غير صحيح',
-    'profile_updated' => 'تم تعديل البيانات الشخصيه بنجاح'
+    'profile_updated' => 'تم تعديل البيانات الشخصيه بنجاح',
+
+    "has_been_by" => 'قام',
+    "created" => 'باضفه',
+    'updated' => 'بتعديل',
+    'deleted' => 'بحذف',
+
+    'Admin' => 'ادمن',
+    'AdminNote' => 'مذكره',
+    'AdminPoint' => 'نقاط',
+    'Exam' => 'امتحان',
+    'Group' => 'صف',
+    'Lesson' => 'درس',
+    'OpenEmis' => 'Open Emis',
+    'Option' => 'اختيارات',
+    'Plan' => 'باقه',
+    'Question' => 'سؤال',
+    'QuestionType' => 'نوع السؤال',
+    'Subject' => 'ماده',
+    'Teacher' => 'مدرس',
+    'TeacherNote' => 'مذكره للمدرس',
+    'TeacherPlan' => 'باقه للمدرس',
+    'Unit' => 'وحده',
+    'User' => 'طالب',
+    'UserNote' => 'مذكره للطالب'
 
 
 ];
